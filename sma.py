90c45853a9d0321ce12eb5f777b14d08f18364db
Abhishek Vijayakumar <vijayakumar.abhishek@gmail.com>
2023-03-17 19:31:07 -0400

    sma class

---
+++ sma.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import time
+
+class SMA:
+
+    def __init__(self, y, p):
+        self.y = y
+        self.p = p
+    def fit(self):
+        print("Fitting", end="")
+        for _ in range(3):
+            time.sleep(1)
+            print(".", end="")
+        print("\nModel fit.")
+        return self
+
+    def forecast(self, k):
+        tmp_data = list(self.y[-self.p:])
+        out = []
+        for i in range(k):
+            out.append(sum(tmp_data[-self.p:]) / self.p)
+            tmp_data.append(out[-1])
+        start_index = self.y.index.max + 1
+        return pd.Series(data=out, name="predicted_mean", index=range(start_index, start_index + k))
