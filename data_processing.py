e12ef11e3c29497634a177be00a925e9a055ea5a
Anna Cai <anana10c@gmail.com>
2023-04-25 00:34:31 -0400

    interpolation

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
+import datetime
 from functools import reduce
 
 
-def load_data():
+def load_data(group="week", interpolation=None):
     df_covid = pd.merge(load_covid_data(count_type="case"),
                         load_covid_data(count_type="death"),
                         on="date")
@@ -15,33 +16,88 @@ def load_data():
                          load_flight_data(terminal="international"),
                          on=["year", "week"])
 
-    df_vaccination = load_vaccination_data()
+    df_vaccination = load_vaccination_data(interpolation=interpolation)
 
-    # standardize dates using ISO calendar weeks (start on Monday)
-    # note that this is not the same as MMWR weeks used by the CDC (start on Sunday)
-    # also note that flight data weeks are ambiguous (no indication of dates by the source)
-    df_covid["year"] = df_covid["date"].dt.isocalendar().year
-    df_covid["week"] = df_covid["date"].dt.isocalendar().week
-    df_search["year"] = df_search["week_start"].dt.isocalendar().year
-    df_search["week"] = df_search["week_start"].dt.isocalendar().week
-    df_vaccination["year"] = df_vaccination["Date"].dt.isocalendar().year
-    df_vaccination["week"] = df_vaccination["Date"].dt.isocalendar().week
+    if group == "week":
+        # standardize dates using ISO calendar weeks (start on Monday)
+        # note that this is not the same as MMWR weeks used by the CDC (start on Sunday)
+        # also note that flight data weeks are ambiguous (no indication of dates by the source)
+        df_covid["year"] = df_covid["date"].dt.isocalendar().year
+        df_covid["week"] = df_covid["date"].dt.isocalendar().week
+        df_search["year"] = df_search["week_start"].dt.isocalendar().year
+        df_search["week"] = df_search["week_start"].dt.isocalendar().week
+        df_vaccination["year"] = df_vaccination["Date"].dt.isocalendar().year
+        df_vaccination["week"] = df_vaccination["Date"].dt.isocalendar().week
 
-    # sum COVID counts and vaccine doses administered by week
-    df_covid = df_covid.groupby(["year", "week"]).sum(numeric_only=True)
-    df_vaccination = df_vaccination.groupby(["year", "week"]).sum(numeric_only=True)
-    df_search.set_index(["year", "week"], inplace=True)
-    df_search.drop(["week_start"], axis=1, inplace=True)
-    df_flight.set_index(["year", "week"], inplace=True)
+        # sum COVID counts and vaccine doses administered by week
+        df_covid = df_covid.groupby(["year", "week"]).sum(numeric_only=True)
+        df_vaccination = df_vaccination.groupby(["year", "week"]).sum(numeric_only=True)
+        df_search.set_index(["year", "week"], inplace=True)
+        df_search.drop(["week_start"], axis=1, inplace=True)
+        df_flight.set_index(["year", "week"], inplace=True)
 
-    df = df_covid.join(df_search).join(df_flight).join(df_vaccination)
+        df = df_covid.join(df_search).join(df_flight).join(df_vaccination)
+    elif group == "day":
+        if interpolation is None:
+            raise ValueError("interpolation cannot be None for daily values,"
+                             "as not all raw data is daily")
+
+        df_vaccination.rename(columns={"Date": "date"}, inplace=True)
+
+        df_search["week_end"] = df_search["week_start"] + datetime.timedelta(days=6)
+        df_flight["week_end"] = df_flight.apply(lambda x: datetime.date.fromisocalendar(x.year, x.week, 7), axis=1) \
+                                         .astype('datetime64[ns]')
+
+        if interpolation == "ffill":
+            df_search["date"] = df_search["week_end"].apply(lambda d: [d + datetime.timedelta(days=i+1) for i in range(7)])
+            df_search = df_search.explode("date", ignore_index=True)
+
+            df_flight["date"] = df_flight["week_end"].apply(lambda d: [d + datetime.timedelta(days=i+1) for i in range(7)])
+            df_flight = df_flight.explode("date", ignore_index=True)
+
+            df_search.drop(["week_start", "week_end"], axis=1, inplace=True)
+            df_flight.drop(["year", "week", "week_end"], axis=1, inplace=True)
+        elif interpolation == "linear":
+            new_index_search = pd.date_range(start=df_search["week_end"].min(),
+                                             end=df_search["week_end"].max(),
+                                             name="date")
+            df_search = df_search.set_index("week_end").reindex(new_index_search).reset_index()
+            df_search_cols = df_search.select_dtypes(include=np.number)
+            for col in df_search_cols:
+                df_search[col] = df_search[col].interpolate()
+
+            new_index_flight = pd.date_range(start=df_flight["week_end"].min(),
+                                             end=df_flight["week_end"].max(),
+                                             name="date")
+            df_flight = df_flight.set_index("week_end").reindex(new_index_flight).reset_index()
+            df_flight_cols = df_flight.select_dtypes(include=np.number)
+            for col in df_flight_cols:
+                df_flight[col] = df_flight[col].interpolate()
+
+            df_search.drop(["week_start"], axis=1, inplace=True)
+            df_flight.drop(["year", "week"], axis=1, inplace=True)
+        else:
+            raise NotImplementedError
+
+        df = df_covid.merge(df_vaccination, on="date", how="left") \
+                     .merge(df_search, on="date", how="left") \
+                     .merge(df_flight, on="date", how="left")
+        df.set_index("date")
+    else:
+        raise NotImplementedError
+
     df["administered_raw"] = df["administered_raw"].fillna(0)
     return df
 
 
-def get_datasets(df, split="strain"):
+def get_datasets(df, split="strain", group="week"):
     if split == "strain":
-        folds = {"alpha": df.iloc[:75], "delta": df.iloc[75:99], "omicron": df.iloc[99:]}
+        if group == "week":
+            folds = {"alpha": df.iloc[:75], "delta": df.iloc[75:99], "omicron": df.iloc[99:]}
+        elif group == "day":
+            raise NotImplementedError
+        else:
+            raise NotImplementedError
         return folds
     else:
         raise NotImplementedError
@@ -103,16 +159,29 @@ def load_flight_data(terminal="global", path=""):
     return df
 
 
-def load_vaccination_data(path=""):
+def load_vaccination_data(path="", interpolation=None):
     if not path:
         path = "data/vaccination/COVID-19_Vaccinations_in_the_United_States_Jurisdiction.csv"
 
     df = pd.read_csv(path, parse_dates=["Date"])
     df = df[df["Location"] == "US"].reset_index(drop=True)
     df = df[["Date", "Administered"]]
+    df.sort_values(by="Date", inplace=True)
+    df.reset_index(drop=True, inplace=True)
+
+    if interpolation is not None:
+        new_index = pd.date_range(start=df["Date"].min(), end=df["Date"].max(), name="Date")
+        df = df.set_index("Date").reindex(new_index).reset_index()
+
+        if interpolation == "ffill":
+            df.fillna(method="ffill", inplace=True)
+        elif interpolation == "linear":
+            df["Administered"] = df["Administered"].interpolate()
+        else:
+            raise NotImplementedError
 
     # subtract series shifted by one to convert from cumulative to raw
-    delta = df["Administered"][:-1].values - df["Administered"][1:].values
+    delta = df["Administered"][1:].values - df["Administered"][:-1].values
     df["administered_raw"] = np.append(delta, 0).astype(np.int64)
     df.drop("Administered", axis=1, inplace=True)
     return df
